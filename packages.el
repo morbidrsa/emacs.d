fe66364ef2dcd11ff13fdd015200fcb6ec27422e
Johannes Thumshirn <johannes.thumshirn@wdc.com>
2024-07-16 08:25:02 +0200

    packages: add elpa to package repos

--- packages.el
+++ packages.el
@@ -1,6 +1,6 @@
 (require 'package)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
 (package-initialize)
 
 (when (or (string= (system-name) "fedora")
