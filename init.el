181234dabda39985f7457c72ec8d7c76ac3254ce
Johannes Thumshirn <jth@kernel.org>
2026-03-26 14:51:39 +0100

    init: add custom.el
    Signed-off-by: Johannes Thumshirn <jth@kernel.org>

--- init.el
+++ init.el
@@ -9,31 +9,12 @@
 (if (eq system-type 'darwin)
     (load "~/.emacs.d/macos"))
 
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(when (file-exists-p custom-file)
+  (load custom-file))
 
 ;;(with-eval-after-load 'mu4e
 ;;  (load "~/.emacs.d/mu4e"))
 (let ((host (car (split-string (system-name) "\\."))))
   (when (string= host "MacBookPro")
     (load "~/.emacs.d/mu4e")))
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("2b0fcc7cc9be4c09ec5c75405260a85e41691abb1ee28d29fcd5521e4fca575b"
-     "a68ec832444ed19b83703c829e60222c9cfad7186b7aea5fd794b79be54146e6"
-     default))
- '(package-selected-packages '(emacs-color-theme-solarized org-mime solarized-theme))
- '(package-vc-selected-packages
-   '((emacs-color-theme-solarized :url
-				  "https://github.com/sellout/emacs-color-theme-solarized.git"
-				  :branch "master"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
